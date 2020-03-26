0ff0670ce86f5e73c98c8e5b80c8e27309a568d8
nlarsen7 <nlarsen2013@gmail.com>
2020-03-26 22:20:40 +0000

    Lab 3.2.1

--- main.go
+++ main.go
@@ -1,9 +1,12 @@
+//Nicholas Larsen
+//March 26, 2020
+//ASCII ART
 package main
 
 import "fmt"
 
 func art() {
-   
+   //creates a function for just the art
     fmt.Println("H    H   EEEEE   L       L       OO")
     fmt.Println("H    H   E       L       L      O  O")
     fmt.Println("HHHHHH   EEEEE   L       L      O  O")
@@ -12,4 +15,5 @@ func art() {
 }
 func main() {
   art()
+  //puts art function under main and prints it all out
 }
