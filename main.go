11bdc42ba61319246363743076da43634e747f0a
nlarsen7 <nlarsen2013@gmail.com>
2020-03-26 22:15:54 +0000

    Initial commit

---
+++ main.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func art() {
+   
+    fmt.Println("H    H   EEEEE   L       L       OO")
+    fmt.Println("H    H   E       L       L      O  O")
+    fmt.Println("HHHHHH   EEEEE   L       L      O  O")
+    fmt.Println("H    H   E       L       L      O  O" )
+    fmt.Println("H    H   EEEEE   LLLLL   LLLLL   OO  ")
+}
+func main() {
+  art()
+}
